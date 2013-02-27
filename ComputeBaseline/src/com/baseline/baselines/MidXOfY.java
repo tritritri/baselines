65758ed18de91f9e224323f69729ef8771d64ad3
Tri Kurniawan Wijaya <tri-kurniawan.wijaya@epfl.ch>
2013-02-27 14:41:45 +0100

    Generalization: MidXOfY

---
+++ ComputeBaseline/src/com/baseline/baselines/MidXOfY.java
@@ -0,0 +1,47 @@
+package com.baseline.baselines;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import com.baseline.util.Util;
+
+public class MidXOfY extends HighXOfY implements Baseline{
+	public MidXOfY () {
+		
+		// as default, take Mid4Of6 configuration
+		WEEKDAY_X = 4;
+		WEEKDAY_Y = 6;
+		
+		WEEKEND_X = 4;
+		WEEKEND_Y = 6;
+	}
+
+	
+	protected void daysRemoval(Calendar targetCal, ArrayList<Double> avgs, ArrayList<Calendar> result) {
+		
+		int X = super.getXDOWType(Util.getDOWType(targetCal));
+
+		while (result.size()>X){
+
+			// remove the min
+			
+			// get the minimum avg
+			int minIdx = Util.getMinIdx(avgs);
+			avgs.remove(minIdx);
+			// remove it from potential baseline's source
+			result.remove(minIdx);
+			
+			if (result.size()>X) {
+				// remove the max
+				// get the maximum avg
+				int maxIdx = Util.getMaxIdx(avgs);
+				avgs.remove(maxIdx);
+				// remove it from potential baseline's source
+				result.remove(maxIdx);
+				
+			}
+		}
+				
+
+	}
+}
