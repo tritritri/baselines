c178096e6e8692e467bdb570549f196b36010b56
matteovasirani <matteo.vasirani@gmail.com>
2013-03-04 17:01:03 +0100

    Removed old data sources for temperature data. Add Wunderground data source for historical and forecast temperature data

--- ComputeBaseline/src/ch/epfl/lsir/wattalyst/weather/WundergroundDataReader.java
+++ ComputeBaseline/src/ch/epfl/lsir/wattalyst/weather/WundergroundDataReader.java
@@ -1,281 +1,163 @@
 package ch.epfl.lsir.wattalyst.weather;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+import java.util.TimeZone;
 
-import org.joda.time.DateTime;
+import org.springframework.web.client.RestTemplate;
 
 import ch.epfl.lsir.wattalyst.baseline.util.SensorReadings;
 import ch.epfl.lsir.wattalyst.baseline.util.Util;
 
-import de.mbenning.weather.wunderground.api.domain.DataSet;
-import de.mbenning.weather.wunderground.api.domain.WeatherStation;
-import de.mbenning.weather.wunderground.impl.services.HttpDataReaderService;
-import de.mbenning.weather.wunderground.impl.services.WeatherStationService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-/**
- * 
- * @author vasirani
- *
- */
 public class WundergroundDataReader {
 
-	private HttpDataReaderService dataReader;
-	private WeatherStationService weatherStationService;
+	private static final String API_ID = "d8a2881e5945445d";
+	private static final String WUNDERGROUND_DATE_FORMAT = "yyyyMMdd";
+	private RestTemplate restTemplate;
 	
 	/**
 	 * 
 	 */
 	public WundergroundDataReader(){
-		dataReader = new HttpDataReaderService();
-		weatherStationService = new WeatherStationService();		
+		restTemplate = new RestTemplate();
 	}
 	
 	/**
 	 * 
-	 * @param weatherDate
-	 * @param weatherStation
+	 * @param startDate
+	 * @param endDate
+	 * @param country
+	 * @param place
 	 * @return
 	 */
-	public double getMinTemperature(Date weatherDate, WeatherStation weatherStation){
-		dataReader.setWeatherDate(weatherDate);
-		dataReader.setWeatherStation(weatherStation);     
-		DataSet minTemp = dataReader.minTemperature();
-		if(minTemp != null){
-			return minTemp.getTemperature();
-		}
-		return Double.NaN;
-	}
+	SensorReadings getHourlyTemperatures(Date startDate, Date endDate, String country, String place) {
 	
-	/**
-	 * 
-	 * @param weatherDate
-	 * @param weatherStation
-	 * @return
-	 */
-	public double getMaxTemperature(Date weatherDate, WeatherStation weatherStation){
-		dataReader.setWeatherDate(weatherDate);
-		dataReader.setWeatherStation(weatherStation);     
-		DataSet maxTemp = dataReader.maxTemperature();
-		if(maxTemp != null){
-			return maxTemp.getTemperature();
-		}
-		return Double.NaN;
-	}
-	
-	/**
-	 * 
-	 * @param weatherDate
-	 * @param weatherStation
-	 * @return
-	 */
-	public double getAvgTemperature(Date weatherDate, WeatherStation weatherStation) {
-		dataReader.setWeatherDate(weatherDate);
-		dataReader.setWeatherStation(weatherStation);     
-		List<DataSet> datasets;
-		try {
-			datasets = dataReader.readDataSets();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return Double.NaN;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return Double.NaN;
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return Double.NaN;
-		}
-		if(!datasets.isEmpty()){
-			Double avgTemp = 0.0;
-			int count = 0;
-			for(DataSet data : datasets){
-				avgTemp = avgTemp + (data.getTemperature() - avgTemp)/(count + 1);
-				count++;
-			}
-			return avgTemp;
-		}
-		return Double.NaN;
-	}
-	
-	/**
-	 * 
-	 * @param weatherDate
-	 * @param weatherStation
-	 * @return
-	 */
-	public double[] getHourlyTemperatures(Date weatherDate, WeatherStation weatherStation) {
-		
 		Calendar calendar = Calendar.getInstance();
-		Util.setToTheBeginningOfTheDay(calendar);		
-		if(weatherDate.equals(calendar.getTime()) || weatherDate.after(calendar.getTime())){
-			throw new RuntimeException("Weather date must be before the present day");
-		}
 		
-		dataReader.setWeatherDate(weatherDate);
-		dataReader.setWeatherStation(weatherStation);     
-		List<DataSet> datasets;
-		try {
-			datasets = dataReader.readDataSets();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ParseException e) {
-			e.printStackTrace();
-			return null;
-		}
-		if(!datasets.isEmpty()){
-			double[] hourlyTemperatures = new double[24];
-			for(int h = 0; h < hourlyTemperatures.length; h++){
-				hourlyTemperatures[h] = Double.NaN;
-			}
-			int[] counters = new int[24];
-			for(DataSet data : datasets){
-				calendar.setTime(data.getDateTime());
-				if(hourlyTemperatures[calendar.get(Calendar.HOUR_OF_DAY)] == Double.NaN)
-					hourlyTemperatures[calendar.get(Calendar.HOUR_OF_DAY)] = 0;
-				hourlyTemperatures[calendar.get(Calendar.HOUR_OF_DAY)] = hourlyTemperatures[calendar.get(Calendar.HOUR_OF_DAY)] + 
-						(data.getTemperature() - hourlyTemperatures[calendar.get(Calendar.HOUR_OF_DAY)])/(counters[calendar.get(Calendar.HOUR_OF_DAY)] + 1);
-				counters[calendar.get(Calendar.HOUR_OF_DAY)]++;
-			}
-			return hourlyTemperatures;
-		}
-		return null;
-	}	
-	
-	/**
-	 * 
-	 * @param weatherStartDate
-	 * @param weatherEndDate
-	 * @param weatherStation
-	 * @return
-	 */
-	public SensorReadings getHourlyTemperatures(Date weatherStartDate, Date weatherEndDate, WeatherStation weatherStation) {
-		
-		// Check dates
-		Calendar calendar = Calendar.getInstance();
-		Util.setToTheBeginningOfTheDay(calendar);		
-		if(weatherStartDate.equals(calendar.getTime()) || weatherStartDate.after(calendar.getTime())){
-			throw new RuntimeException("Weather start date must be before the present day");
-		}
-		
-		if(weatherEndDate.equals(calendar.getTime()) || weatherEndDate.after(calendar.getTime())){
-			throw new RuntimeException("Weather end date must be before the present day");
-		}
-		
-		if(weatherStartDate.after(weatherEndDate)){
-			throw new RuntimeException("Start date must be before or equal to end date");
-		}
-		
-		SensorReadings hourlyTemperatures = new SensorReadings();
-				
-		calendar.setTime(weatherEndDate);
-		Util.setToTheEndOfTheDay(calendar);
-		weatherEndDate = calendar.getTime();
-		
-		Date weatherDate = weatherStartDate;
-		calendar.setTime(weatherDate);
+		// Set today date
 		Util.setToTheBeginningOfTheDay(calendar);
-		weatherDate = calendar.getTime();
-				
-		// Retrieve hourly temperatures for all the days between start and end
-		while(weatherDate.before(weatherEndDate) || weatherDate.equals(weatherEndDate)){
-			double[] hourlyTemperaturesForDay = getHourlyTemperatures(weatherDate, weatherStation);
-			if(hourlyTemperaturesForDay != null){
-				for(int h = 0; h < hourlyTemperaturesForDay.length; h++){
-					calendar.set(Calendar.HOUR_OF_DAY, h);
-					hourlyTemperatures.insert(calendar.getTimeInMillis(), hourlyTemperaturesForDay[h]);
+		Date today = calendar.getTime();
+		
+		// Set start date
+		calendar.setTime(startDate);
+		Util.setToTheBeginningOfTheDay(calendar);
+		startDate = calendar.getTime();
+		
+		// Set end date
+		calendar.setTime(endDate);
+		Util.setToTheEndOfTheDay(calendar);
+		endDate = calendar.getTime();
+		
+		// Initialize current day
+		Date currentDate = startDate;
+		calendar.setTime(currentDate);
+		SimpleDateFormat formatter = new SimpleDateFormat(WUNDERGROUND_DATE_FORMAT);
+		SensorReadings hourlyTemperatures = new SensorReadings();
+		
+		// Cycle over all the days using "history_YYYYMMDD"
+		JsonParser parser = new JsonParser();
+		while(currentDate.before(endDate)){
+			String JSONresult = restTemplate.getForObject("http://api.wunderground.com/api/{API_ID}/history_{date}/q/{country}/{place}.json",
+					String.class, API_ID, formatter.format(currentDate), country, place);
+			JsonObject obj = parser.parse(JSONresult).getAsJsonObject();
+			if(obj.has("history")){
+				JsonObject history = obj.get("history").getAsJsonObject();
+				if(history.has("observations")){
+					JsonArray observations = history.get("observations").getAsJsonArray();
+					for(JsonElement obs : observations){
+						JsonObject date = obs.getAsJsonObject().get("utcdate").getAsJsonObject();
+						int year = date.get("year").getAsInt();
+						int month = date.get("mon").getAsInt();
+						int day = date.get("mday").getAsInt();
+						int hour = date.get("hour").getAsInt();
+						double temp = obs.getAsJsonObject().get("tempm").getAsDouble();		
+						temp = (temp == -9999 ? Double.NaN : temp);
+						
+						Calendar utcCal = Calendar.getInstance();
+						utcCal.setTimeZone(TimeZone.getTimeZone("UTC"));	
+						utcCal.set(Calendar.YEAR, year);
+						utcCal.set(Calendar.MONTH, month - 1);
+						utcCal.set(Calendar.DAY_OF_MONTH, day);
+						utcCal.set(Calendar.HOUR_OF_DAY, hour);
+						utcCal.set(Calendar.MINUTE, 0);
+						utcCal.set(Calendar.SECOND, 0);
+						utcCal.set(Calendar.MILLISECOND, 0);
+						
+						hourlyTemperatures.insert(utcCal.getTimeInMillis(), temp);
+					}
 				}
 			}
-			// If for a given day there are no data, fill with Double.NaN
-			else{
-				for(int h = 0; h < 24; h++){
-					calendar.set(Calendar.HOUR_OF_DAY, h);
-					hourlyTemperatures.insert(calendar.getTimeInMillis(), Double.NaN);
-				}
-			}
-			calendar.setTime(weatherDate);
+			calendar.setTime(currentDate);
 			calendar.add(Calendar.DAY_OF_YEAR, 1);
-			weatherDate = calendar.getTime();
+			currentDate = calendar.getTime();
 		}
+		
+		// If end date is today or in the future, invoke also "hourly10day"
+		if(endDate.equals(today) || endDate.after(today)){
+		
+			String JSONresult = restTemplate.getForObject("http://api.wunderground.com/api/{API_ID}/hourly10day/geolookup/q/{country}/{place}.json",
+					String.class, API_ID, country, place);
+			JsonObject obj = parser.parse(JSONresult).getAsJsonObject();
+			
+			// Get the time zone 
+			if(obj.has("location")){
+				JsonObject location = obj.get("location").getAsJsonObject();
+				String tz_long = location.get("tz_long").getAsString();
+				TimeZone timeZone = TimeZone.getTimeZone(tz_long);
+				
+				if(obj.has("hourly_forecast")){
+					JsonArray hourly_forecast = obj.get("hourly_forecast").getAsJsonArray();
+					
+					Calendar localCalendar = Calendar.getInstance();
+					localCalendar.setTimeZone(timeZone);
+					
+					for(JsonElement forecast : hourly_forecast){
+						JsonObject FCTTIME = forecast.getAsJsonObject().get("FCTTIME").getAsJsonObject();
+						int year = FCTTIME.get("year").getAsInt();
+						int month = FCTTIME.get("mon").getAsInt();
+						int day = FCTTIME.get("mday").getAsInt();
+						int hour = FCTTIME.get("hour").getAsInt();
+						
+						localCalendar.set(Calendar.YEAR, year);
+						localCalendar.set(Calendar.MONTH, month - 1);
+						localCalendar.set(Calendar.DAY_OF_MONTH, day);
+						localCalendar.set(Calendar.HOUR_OF_DAY, hour);
+						localCalendar.set(Calendar.MINUTE, 0);
+						localCalendar.set(Calendar.SECOND, 0);
+						localCalendar.set(Calendar.MILLISECOND, 0);
+						
+						if(localCalendar.getTime().after(endDate)){
+							break;
+						}
+						JsonObject temperature = forecast.getAsJsonObject().get("temp").getAsJsonObject();
+						double temp = temperature.get("metric").getAsDouble();
+						temp = (temp == -9999 ? Double.NaN : temp);
+						
+						hourlyTemperatures.insert(localCalendar.getTimeInMillis(), temp);
+					}
+				}
+			}
+		}
+		
 		return hourlyTemperatures;
 	}
 	
 	/**
 	 * 
-	 * @param weatherStartDate
-	 * @param weatherEndDate
-	 * @param weatherStation
-	 * @param filename
-	 * @return
-	 * @throws IOException 
-	 */
-	public void writeHourlyTemperatures(Date weatherStartDate, Date weatherEndDate, WeatherStation weatherStation, String filename) throws IOException {
-		
-		SensorReadings hourlyTemperatures = getHourlyTemperatures(weatherStartDate, weatherEndDate, weatherStation);
-		
-		if(weatherStartDate.after(weatherEndDate)){
-			throw new RuntimeException("Start date must be before or equal to end date");
-		}
-		
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(filename)));
-		bw.write(hourlyTemperatures.toStringAsc());
-		bw.close();
-	}
-	
-	/**
-	 * 
-	 * @param country
-	 * @param city
-	 * @return
-	 */
-	public WeatherStation getWeatherStation(String country, String city){
-		// find all weather stations
-		List<WeatherStation> stations = weatherStationService.findAllWeatherStationsByCountry(country);
-		for(WeatherStation station : stations){
-			if(station.getCity().equals(city)){
-				return station;
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * 
 	 * @param args
+	 * @throws ParseException 
 	 */
-	public static void main(String args[]) {
-				
-		WundergroundDataReader wundergroundDataReader = new WundergroundDataReader();
-		WeatherStation weatherStation = wundergroundDataReader.getWeatherStation("Sweden", "Lulea");
-		Date weatherDate = new DateTime(2013, 2, 25, 0, 0, 0).toDate();
-		double min = wundergroundDataReader.getMinTemperature(weatherDate, weatherStation);
-		double max = wundergroundDataReader.getMaxTemperature(weatherDate, weatherStation);
-		double avg = wundergroundDataReader.getAvgTemperature(weatherDate, weatherStation);
-		double[] hourlyTemperatures = wundergroundDataReader.getHourlyTemperatures(weatherDate, weatherStation);
-		
-		System.out.println("min:max temperature at " + weatherStation.getCity() + " ID " + weatherStation.getStationId() +
-						" on " + weatherDate + " is [" + min + ":" + max + "]");	
-		System.out.println("avg temperature at " + weatherStation.getCity() + " ID " + weatherStation.getStationId() +
-				" on " + weatherDate + " is " + avg);
-		System.out.print("hourly temperatures at " + weatherStation.getCity() + " ID " + weatherStation.getStationId() +
-				" on " + weatherDate + " is [ ");
-		for(double t : hourlyTemperatures){
-			System.out.print(t + " ");
-		}
-		System.out.println("]");
-		
-		Date weatherStartDate = new DateTime(2012, 1, 1, 0, 0, 0).toDate();
-		Date weatherEndDate = new DateTime(2012, 12, 31, 0, 0, 0).toDate();
-		System.out.println(wundergroundDataReader.getHourlyTemperatures(weatherStartDate, weatherEndDate, weatherStation).toStringAsc());
+	public static void main(String[] args) throws ParseException{
+		WundergroundDataReader r = new WundergroundDataReader();
+		Date startDate = new SimpleDateFormat(WUNDERGROUND_DATE_FORMAT).parse("20130304");
+		Date endDate = new SimpleDateFormat(WUNDERGROUND_DATE_FORMAT).parse("20130305");
+		System.out.println(r.getHourlyTemperatures(startDate, endDate, "Sweden", "Lulea").toStringAsc());
 	}
 }
